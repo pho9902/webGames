590fe4cea419dda2212e53b4e9ccca3c36cef58f
unknown <pho9902@naver.com>
2022-02-24 18:46:08 +0900

    wrongWord : 좌측 공백 부여

--- src/components/WrongWord/index.tsx
+++ src/components/WrongWord/index.tsx
@@ -8,18 +8,20 @@ export default function WrongWord() {
   return (
     <Wrap>
       <h2>다른글자 찾기</h2>
-      <h3>레벨선택</h3>
       <Level>
-        {Array(5)
-          .fill(0)
-          .map((_, idx) => (
-            <CommonOutLineBtn
-              key={idx}
-              onClick={() => router.push(`/wrongword/${idx + 1}`)}
-            >
-              레벨 {idx + 1}
-            </CommonOutLineBtn>
-          ))}
+        <h3>레벨선택</h3>
+        <div>
+          {Array(5)
+            .fill(0)
+            .map((_, idx) => (
+              <CommonOutLineBtn
+                key={idx}
+                onClick={() => router.push(`/wrongword/${idx + 1}`)}
+              >
+                레벨 {idx + 1}
+              </CommonOutLineBtn>
+            ))}
+        </div>
       </Level>
     </Wrap>
   );
