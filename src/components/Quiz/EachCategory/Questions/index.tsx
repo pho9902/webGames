317319ceeec1734e020f1eea1619a2531b044e6f
unknown <pho9902@naver.com>
2022-03-07 15:54:02 +0900

    quiz : 오류수정

--- src/components/Quiz/EachCategory/Questions/index.tsx
+++ src/components/Quiz/EachCategory/Questions/index.tsx
@@ -2,27 +2,32 @@ import { SUBJECT_QUESTION } from "components/common/QuizDatas/javascript";
 import { useState } from "react";
 import { CommonOutLineBtn } from "styles/button";
 
-interface Props {
+interface functionProps {
   answer: string;
   index: number;
 }
+
 export default function Questions() {
   const [level, setLevel] = useState(0);
   const [answer, setAnswer] = useState("");
 
-  function isAnswer({ answer, index }: Props) {
-    if (answer === SUBJECT_QUESTION[index]) return true;
+  function isAnswer(answer: string, index: number) {
+    setAnswer("");
+    if (answer === SUBJECT_QUESTION[1].answers[index]) return true;
+    return false;
   }
 
   return (
     <div>
-      {SUBJECT_QUESTION[level].questions.map((el, idx) => {
+      {SUBJECT_QUESTION[level].questions.map((el: string, index: number) => {
         return (
-          <div>
+          <div key={index}>
             <span>{el}</span>
-            <input onChange={e => setAnswer(e.target.value)} />
+            <input value={answer} onChange={e => setAnswer(e.target.value)} />
             <CommonOutLineBtn
-              onClick={() => isAnswer(answer, idx) && alert("정답")}
+              onClick={() =>
+                isAnswer(answer, index) ? alert("정답") : alert("오답")
+              }
             >
               제출
             </CommonOutLineBtn>
--- src/components/common/QuizDatas/javascript.ts
+++ src/components/common/QuizDatas/javascript.ts
@@ -1,6 +1,6 @@
 export const MULTIPLE_CHOICE_QUESTION = [];
 
-export const SUBJECT_QUESTION: {}[] = [
-  { questions: ["내 이름은?", "개발 지원 분야는?"] },
-  { answers: ["박현우", "프론트"] },
+export const SUBJECT_QUESTION: { [key: string]: any }[] = [
+  { questions: ["내 이름은?", "개발 지원 분야는?", "내 나이는?"] },
+  { answers: ["박현우", "프론트", "24"] },
 ];
