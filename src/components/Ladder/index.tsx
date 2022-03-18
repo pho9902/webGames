778f6e9b606ed842241966323f4f800a8994f542
박현우 <pho9902@naver.com>
2022-03-18 22:22:12 +0900

    ladder : 폴더, 컴포넌트 이름 명시적으로 변경

--- src/components/Ladder/index.tsx
+++ src/components/Ladder/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Wrap } from "./style";
-import Before from "./Before";
-import After from "./After";
+import SettingGame from "./SettingGame";
+import GameBoard from "./GameBoard";
 
 export default function Ladder() {
   const [count, setCount] = useState<number>(2);
@@ -10,10 +10,10 @@ export default function Ladder() {
   return (
     <Wrap>
       {isPlay ? (
-        // <After count={count} />
+        // <GameBoard count={count} />
         <div>사다리</div>
       ) : (
-        <Before count={count} setCount={setCount} setIsPlay={setIsPlay} />
+        <SettingGame count={count} setCount={setCount} setIsPlay={setIsPlay} />
       )}
     </Wrap>
   );
