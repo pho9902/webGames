eac53b76ec219de4e5cc868497fc9475ba012468
박현우 <pho9902@naver.com>
2022-03-20 20:17:28 +0900

    ladder : 사다리 게임 컴포넌트 아이콘 return, 스타일 생성

---
+++ src/components/Ladder/GameBoard/style.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+    display: flex;
+`
+
+export const Ladder = styled.div`
+    border-left: 1px solid black;
+    border-top : none;
+    border-bottom : none;
+    height: 500px;
+    width: 40px;
+`
+
+export const EachIcon = styled.div`
+    heignt: 500px;
+    // border: 1px solid black;
+`
+
+export const Goal = styled.button`
+
+`
