40b6eb14dd8f5acf902fa7f2709b905e97ffe652
unknown <pho9902@naver.com>
2022-03-21 11:21:32 +0900

    ladder : 게임시작 전 상태 추가

--- src/components/Ladder/GameBoard/style.ts
+++ src/components/Ladder/GameBoard/style.ts
@@ -1,22 +1,34 @@
 import styled from "styled-components";
 
 export const Wrap = styled.div`
-    display: flex;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Ladder = styled.div`
-    border-left: 1px solid black;
-    border-top : none;
-    border-bottom : none;
-    height: 500px;
-    width: 40px;
-`
+  border-left: 1px solid black;
+  border-top: none;
+  border-bottom: none;
+  height: 400px;
+  width: 40px;
+`;
 
 export const EachIcon = styled.div`
-    heignt: 500px;
-    // border: 1px solid black;
-`
+  /* height: 500px; */
+  // border: 1px solid black;
+`;
 
-export const Goal = styled.button`
+export const Goal = styled.input``;
 
-`
+export const Top = styled.div`
+  display: flex;
+`;
+
+export const Modal = styled.div`
+  width: 1000px;
+  height: 300px;
+  background: white;
+  z-index: 9;
+  position: relative;
+  bottom: 430px;
+`;
